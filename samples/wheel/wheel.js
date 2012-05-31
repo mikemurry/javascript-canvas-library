e6a7b2ad25d036594967043221ea90de1a57471d
Mike <mikemurry@gmail.com>
2012-05-31 12:34:14 -0700

    Made sizing based on browser window instead of fixed.

--- samples/wheel/wheel.js
+++ samples/wheel/wheel.js
@@ -16,10 +16,7 @@ var web = {
             zindex: 0
         });
 
-        this.center = {
-            x: Math.floor(this.canvas.width / 2),
-            y: Math.floor(this.canvas.height / 2)
-        };
+        this.center = new VENT.Point(Math.floor(this.canvas.width / 2), Math.floor(this.canvas.height / 2));
 
         // Instantiate Data
         this.pointCount = 600;
@@ -27,18 +24,32 @@ var web = {
         this.hover = null;
         this.activity = [];
         this.strength = [];
+        this.lineSize = 2;
+
+        // Calculate Sizes
+        if (this.canvas.width < this.canvas.height) {
+            this.outerRing = Math.floor(this.canvas.width * 0.9 / 2);
+            this.innerRing = Math.floor(this.canvas.width * 0.5 / 2);
+            this.ringSize = Math.floor((this.outerRing - this.innerRing) / 4);
+        } else {
+            this.outerRing = Math.floor(this.canvas.height * 0.9 / 2);
+            this.innerRing = Math.floor(this.canvas.height * 0.5 / 2);
+            this.ringSize = Math.floor((this.outerRing - this.innerRing) / 4);
+        }
+
+        if (this.ringSize < 30) { this.lineSize = 1; }
 
         // Generate Data
         for (i = 0; i<this.pointCount; i++) {
-            this.activity.push(this.calculateStack(this.center, this.pointCount, i, 300, 250, "sin"));
-            this.strength.push(this.calculateStack(this.center, this.pointCount, i, -100, 249, "random"));
+            this.activity.push(this.calculateStack(this.center, this.pointCount, i, this.ringSize * 3, this.innerRing + this.ringSize, "sin"));
+            this.strength.push(this.calculateStack(this.center, this.pointCount, i, this.ringSize * -1, this.innerRing + this.ringSize - 1, "random"));
         }
 
         // Enable Performance Monitoring
         perf.init("vent-fps-value");
 
         // Initialize Mouse
-        this.mouse = { x: 0, y: 0 };
+        this.mouse = new VENT.Point(0,0);
 
         // Register Render Function
         VENT.renderer.registerRender(function() {
@@ -56,16 +67,13 @@ var web = {
 
     handleMouse: function handleMouse(e) {
 
-        var distance = VENT.utilities.pointDistance(this.mouse, this.center);
+        var distance = this.mouse.distance(this.center);
 
-        this.mouse = {
-            x: e.clientX,
-            y: e.clientY
-        };
+        this.mouse.set(e.clientX, e.clientY);
 
         if (distance <= 400 && distance >= 200) {
-            var utils = VENT.utilities;
-            this.hover = Math.round((utils.degrees(utils.pointAngle(this.mouse, this.center)) + 180) / (360/this.pointCount));
+            var degrees = VENT.utilities.degrees;
+            this.hover = Math.round((degrees(this.mouse.angle(this.center)) + 180) / (360/this.pointCount));
             if (this.hover === this.pointCount) { this.hover = 0; }
         } else { this.hover = null; }
 
@@ -77,16 +85,16 @@ var web = {
         this.canvas.clear();
 
         // Draw Lines
-        this.canvas.drawHaystack(this.activity, "rgba(132,216,77,1)", 2);
-        this.canvas.drawHaystack(this.strength, "rgba(100,100,100,1)", 2);
+        this.canvas.drawHaystack(this.activity, "rgba(132,216,77,1)", this.lineSize);
+        this.canvas.drawHaystack(this.strength, "rgba(100,100,100,1)", this.lineSize);
 
         // Increment Step
         this.step++;
         if (this.step >= this.activity.length) { this.step = 0; }
 
         // Draw Values
-        this.activity[this.step] = this.calculateStack(this.center, this.pointCount, this.step, 300, 250, "sin");
-        this.strength[this.step] = this.calculateStack(this.center, this.pointCount, this.step, -100, 249, "random");
+        this.activity[this.step] = this.calculateStack(this.center, this.pointCount, this.step, this.ringSize * 3, this.innerRing + this.ringSize, "sin");
+        this.strength[this.step] = this.calculateStack(this.center, this.pointCount, this.step, this.ringSize * -1, this.innerRing + this.ringSize - 1, "random");
 
         // Draw Step Indicator
         this.canvas.drawPath([this.center, this.activity[this.step][0]], "rgba(255,255,255,1)", 2);
@@ -101,8 +109,6 @@ var web = {
     calculateStack: function calculateStack(center, points, step, maxSize, radius, type) {
 
         var size, a, ang, c;
-
-        var tangent = VENT.utilities.pointTangent;
         var degrees = VENT.utilities.degrees;
 
         if (type === "random") {
@@ -111,22 +117,21 @@ var web = {
             size = (maxSize * 0.5) +  ((Math.sin(VENT.performance.elapsed() * 2) * Math.cos(VENT.performance.elapsed())) * (maxSize * 0.4)) + (maxSize * 0.1 * Math.random());
         }
 
-        a = tangent(center, step * (360/points), radius);
-        ang = Math.atan2(center.y - a.y, center.x - a.x);
-        c = tangent(a, degrees(ang), size / -2);
+        a = center.tangent(step * (360/points), radius);
+        ang = center.angle(a);
+        c = a.tangent(degrees(ang), size);
 
         return [c, a];
 
     },
 
     drawAxis: function drawAxis(center) {
-        console.log(this);
         this.axis.drawCircle(center, 10, "rgba(132,216,77,0.5)");
         this.axis.drawCircle(center, 5, "rgba(255,255,255,1)");
-        this.axis.drawCircle(center, 200, null, "rgba(255,255,255,0.2)", 1);
-        this.axis.drawCircle(center, 300, null, "rgba(255,255,255,0.2)", 1);
-        this.axis.drawCircle(center, 350, null, "rgba(255,255,255,0.2)", 1);
-        this.axis.drawCircle(center, 400, null, "rgba(255,255,255,0.2)", 1);
+        this.axis.drawCircle(center, this.innerRing, null, "rgba(255,255,255,0.2)", 1);
+        this.axis.drawCircle(center, this.innerRing + (this.ringSize * 2), null, "rgba(255,255,255,0.2)", 1);
+        this.axis.drawCircle(center, this.innerRing + (this.ringSize * 3), null, "rgba(255,255,255,0.2)", 1);
+        this.axis.drawCircle(center, this.innerRing + (this.ringSize * 4), null, "rgba(255,255,255,0.2)", 1);
     }
 
 };
